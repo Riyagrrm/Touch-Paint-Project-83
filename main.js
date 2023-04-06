9b1338a533f3752d83de8565749677372d09d3a3
Riyagrrm <120349265+Riyagrrm@users.noreply.github.com>
2023-04-06 11:28:00 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
 canvas=document.getElementById("myCanvas")
-getContext.getContext("2d")
-var colour=""
+ctx=canvas.getContext("2d")
+var color=""
 var width=""
 var cx
 var cy
 var lx
 var ly
+var mouseEvent=""
 canvas.addEventListener("mousedown", my_mousedown);
-function my_mousedown(e);
+function my_mousedown(e)
 {
     color=document.getElementById("color").value;
-    width=document.getElementById("lineWidth").value;
+    width=document.getElementById("width").value;
     mouseEvent="mousedown";
 }
 canvas.addEventListener("mouseup", my_mouseup);
-function my_mouseup(e);
+function my_mouseup(e)
 {
     mouseEvent="mouseup";
 }
 canvas.addEventListener("mouseleave", my_mouseleave);
-function my_mouseleave(e);
+function my_mouseleave(e)
 {
     mouseEvent="mouseleave";
 }
 canvas.addEventListener("mousemove", my_mousemove);
-function my_mousemove(e);
+function my_mousemove(e)
 {
-    mouseEvent="mousemove";
-    cx = e.clientX - canvas.offSetLeft;
-    cy = e.clientY - canvas.offSetTop;
-    if (mouseEvent=="mouseDown")
+    cx = e.clientX - canvas.offsetLeft;
+    cy = e.clientY - canvas.offsetTop;
+    if (mouseEvent=="mousedown")
     {
+        console.log("inside if condition")
         ctx.beginPath()
         ctx.strokeStyle=color
         ctx.lineWidth=width
@@ -50,12 +51,12 @@ canvas.addEventListener("touchstart", my_touchstart);
 canvas.addEventListener("touchmove",my_touchmove)
 function my_touchmove(e)
 {
-    current_touch_x=e.touches[0].clientX-canvas.offSetLeft
-    current_touch_y=e.touches[0].clientY-canvas.offSetTop
+    current_touch_x=e.touches[0].clientX-canvas.offsetLeft
+    current_touch_y=e.touches[0].clientY-canvas.offsetTop
     ctx.beginPath()
     ctx.strokeStyle=color
     ctx.lineWidth=width
-    ctx.moveTo(last_touuch_x, last_touch_y)
+    ctx.moveTo(lx, ly)
     ctx.lineTo(current_touch_x, current_touch_y)
     ctx.stroke()
 }
